d8a898a37bb1b66b88cda2d5d3b90e3ac4fd7b26
Dick <yeahydq@gmail.com>
2025-03-19 08:35:48 +0800

    feat: add configuration files and initial implementations for multiple cloud functions

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -17,11 +17,16 @@ declare global {
   }
 
   type IUserInfo = {
-    nickname?: string
-    avatar?: string
+    nickName?: string
+    avatarUrl?: string
     /** 微信的 openid，非微信没有这个字段 */
     openid?: string
     token?: string
+    id?: string
+    manager?: boolean
+    phone?: string
+    address?: string
+    email?: string
   }
 }
 
