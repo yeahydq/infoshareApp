9ffe1bc07394d49b1e8cabfda60ff3325a1a4324
Dick <yeahydq@gmail.com>
2025-03-23 23:59:15 +0800

    feat: refactor navigation functions to use a centralized routing object

--- src/pages/weshares/personal-center/index.vue
+++ src/pages/weshares/personal-center/index.vue
@@ -20,18 +20,18 @@
           <view class="user-id">ID:HY000054</view>
         </view>
       </view>
-      <view class="edit-button" @click="navigateToPersonalInfo">
+      <view class="edit-button" @click="navigateTo(NavigationRoutes.PERSONAL_INFO)">
         <text>编辑资料</text>
       </view>
     </view>
 
     <view class="service-cards">
-      <view class="service-card" @click="navigateToFindTeachers">
+      <view class="service-card" @click="navigateTo(NavigationRoutes.FIND_TEACHERS)">
         <view class="service-icon">🔍</view>
         <view class="service-text">帮我找</view>
         <view class="service-icon-arrow">></view>
       </view>
-      <view class="service-card" @click="navigateToTeacherRegistration">
+      <view class="service-card" @click="navigateTo(NavigationRoutes.TEACHER_REGISTRATION)">
         <view class="service-icon">👩‍🏫</view>
         <view class="service-text">教员入驻</view>
         <view class="service-icon-arrow">></view>
@@ -43,41 +43,41 @@
     </view>
 
     <view class="features-grid">
-      <view class="feature-item" @click="navigateToQrCode">
+      <view class="feature-item" @click="navigateTo(NavigationRoutes.QR_CODE)">
         <view class="feature-icon">📋</view>
         <view class="feature-text">推广二维码</view>
       </view>
-      <view class="feature-item" @click="navigateToHelpCenter">
+      <view class="feature-item" @click="navigateTo(NavigationRoutes.HELP_CENTER)">
         <view class="feature-icon">❓</view>
         <view class="feature-text">帮助中心</view>
       </view>
-      <view class="feature-item" @click="navigateToAddress">
+      <view class="feature-item" @click="navigateTo(NavigationRoutes.ADDRESS)">
         <view class="feature-icon">📍</view>
         <view class="feature-text">我的地址</view>
       </view>
-      <view class="feature-item" @click="navigateToAnnouncement">
+      <view class="feature-item" @click="navigateTo(NavigationRoutes.ANNOUNCEMENT)">
         <view class="feature-icon">🔔</view>
         <view class="feature-text">系统公告</view>
       </view>
     </view>
 
     <view class="info-links">
-      <view class="info-link" @click="navigateToAboutUs">
+      <view class="info-link" @click="navigateTo(NavigationRoutes.ABOUT_US)">
         <view class="info-icon">ℹ️</view>
         <view class="info-text">关于我们</view>
         <view class="info-arrow">></view>
       </view>
-      <view class="info-link" @click="navigateToCustomerService">
+      <view class="info-link" @click="navigateTo(NavigationRoutes.CUSTOMER_SERVICE)">
         <view class="info-icon">🎧</view>
         <view class="info-text">在线客服</view>
         <view class="info-arrow">></view>
       </view>
-      <view class="info-link" @click="navigateToContactUs">
+      <view class="info-link" @click="navigateTo(NavigationRoutes.CONTACT_US)">
         <view class="info-icon">📞</view>
         <view class="info-text">联系我们</view>
         <view class="info-arrow">></view>
       </view>
-      <view class="info-link" @click="navigateToSettings">
+      <view class="info-link" @click="navigateTo(NavigationRoutes.SETTINGS)">
         <view class="info-icon">⚙️</view>
         <view class="info-text">系统设置</view>
         <view class="info-arrow">></view>
@@ -85,15 +85,15 @@
     </view>
     <!-- 
     <view class="bottom-nav">
-      <view class="nav-item" @click="navigateToHome">
+      <view class="nav-item" @click="navigateTo(NavigationRoutes.HOME)">
         <view class="nav-icon">🏠</view>
         <view class="nav-text">首页</view>
       </view>
-      <view class="nav-item" @click="navigateToFindTeachers">
+      <view class="nav-item" @click="navigateTo(NavigationRoutes.FIND_TEACHERS)">
         <view class="nav-icon">👨‍🏫</view>
         <view class="nav-text">找老师</view>
       </view>
-      <view class="nav-item" @click="navigateToOrders">
+      <view class="nav-item" @click="navigateTo(NavigationRoutes.ORDERS)">
         <view class="nav-icon">📝</view>
         <view class="nav-text">订单</view>
       </view>
@@ -106,94 +106,29 @@
 </template>
 
 <script setup>
-// Navigation functions
-function navigateToFindTeachers() {
-  console.log('Navigating to Find Teachers page')
-  uni.navigateTo({
-    url: '../find-teachers/index',
-  })
+const NavigationRoutes = {
+  FIND_TEACHERS: '../find-teachers/index',
+  TEACHER_REGISTRATION: '../teacher-registration/index',
+  PERSONAL_INFO: '../personal-info/index',
+  QR_CODE: '../qr-code/index',
+  HELP_CENTER: '../help-center/index',
+  ADDRESS: '../address-management/index',
+  ANNOUNCEMENT: '../system-announcement/index',
+  ABOUT_US: null, // This page doesn't exist yet
+  CUSTOMER_SERVICE: null, // This page doesn't exist yet
+  CONTACT_US: null, // This page doesn't exist yet
+  SETTINGS: '../system-settings/index',
+  HOME: '../index/index',
+  ORDERS: '../index/index', // This page doesn't exist yet, so navigate to home
 }
 
-function navigateToTeacherRegistration() {
-  console.log('Navigating to Teacher Registration page')
-  uni.navigateTo({
-    url: '../teacher-registration/index',
-  })
-}
-
-function navigateToPersonalInfo() {
-  console.log('Navigating to Teacher Registration page')
-  uni.navigateTo({
-    url: '../personal-info/index',
-  })
-}
-
-function navigateToQrCode() {
-  console.log('Navigating to QR Code page')
-  uni.navigateTo({
-    url: '../qr-code/index',
-  })
-}
-
-function navigateToHelpCenter() {
-  console.log('Navigating to Help Center page')
-  uni.navigateTo({
-    url: '../help-center/index', // 在当前页面直接重定向到另一个页面
-  })
-  // uni.navigateTo({
-  //   url: '../help-center/index',
-  // })
-}
-
-function navigateToAddress() {
-  console.log('Navigating to Address Management page')
-  uni.navigateTo({
-    url: '../address-management/index',
-  })
-}
-
-function navigateToAnnouncement() {
-  console.log('Navigating to System Announcement page')
-  uni.navigateTo({
-    url: '../system-announcement/index',
-  })
-}
-
-function navigateToAboutUs() {
-  console.log('Navigating to About Us page')
-  // This page doesn't exist yet, so just log the action
-}
-
-function navigateToCustomerService() {
-  console.log('Navigating to Customer Service page')
-  // This page doesn't exist yet, so just log the action
-}
-
-function navigateToContactUs() {
-  console.log('Navigating to Contact Us page')
-  // This page doesn't exist yet, so just log the action
-}
-
-function navigateToSettings() {
-  console.log('Navigating to Settings page')
-  uni.navigateTo({
-    url: '../system-settings/index',
-  })
-}
-
-function navigateToHome() {
-  console.log('Navigating to Home page')
-  uni.navigateTo({
-    url: '../index/index',
-  })
-}
-
-function navigateToOrders() {
-  console.log('Navigating to Orders page')
-  // This page doesn't exist yet, so navigate to home
-  uni.navigateTo({
-    url: '../index/index',
-  })
+function navigateTo(route) {
+  if (route) {
+    console.log(`Navigating to ${route}`)
+    uni.navigateTo({ url: route })
+  } else {
+    console.log('This page does not exist yet')
+  }
 }
 </script>
 
